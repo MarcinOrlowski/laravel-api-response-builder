d6e8011dec74e594d1d9635a663330f656f9bc7a
MarcinOrlowski <mail@MarcinOrlowski.com>
2017-02-20 01:34:09 +0100

    Code cleanup

--- tests/ResponseBuilderTestCase.php
+++ tests/ResponseBuilderTestCase.php
@@ -1,7 +1,15 @@
 <?php
 
+
 /**
- * Class TestCase
+ * Laravel API Response Builder
+ *
+ * @package   MarcinOrlowski\ResponseBuilder
+ *
+ * @author    Marcin Orlowski <mail (#) marcinorlowski (.) com>
+ * @copyright 2016-2017 Marcin Orlowski
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/MarcinOrlowski/laravel-api-response-builder
  */
 
 use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
--- tests/TestCaseBase.php
+++ tests/TestCaseBase.php
@@ -1,7 +1,14 @@
 <?php
 
 /**
- * Class TestCase
+ * Laravel API Response Builder
+ *
+ * @package   MarcinOrlowski\ResponseBuilder
+ *
+ * @author    Marcin Orlowski <mail (#) marcinorlowski (.) com>
+ * @copyright 2016-2017 Marcin Orlowski
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/MarcinOrlowski/laravel-api-response-builder
  */
 
 use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
@@ -19,12 +26,13 @@ abstract class TestCaseBase extends Orchestra\Testbench\TestCase
 	 *   $obj = new \App\Foo();
 	 *   $result = $method->invokeArgs($obj, ...);
 	 *
-	 * @param string $class_name name of the class method belongs to, i.e. "Bar". Can be namespaced i.e. "Foo\Bar" (no starting backslash)
+	 * @param string $class_name  name of the class method belongs to, i.e. "Bar". Can be namespaced i.e. "Foo\Bar" (no starting backslash)
 	 * @param string $method_name method name to call
 	 *
 	 * @return \ReflectionMethod
 	 */
-	protected function getProtectedMethod($class_name, $method_name) {
+	protected function getProtectedMethod($class_name, $method_name)
+	{
 		$class = new \ReflectionClass($class_name);
 		$method = $class->getMethod($method_name);
 		$method->setAccessible(true);
@@ -32,8 +40,20 @@ abstract class TestCaseBase extends Orchestra\Testbench\TestCase
 		return $method;
 	}
 
-	protected function getRandomString($prefix='rand') {
-		return $prefix . '_' . md5(uniqid(mt_rand(), true));
+	/**
+	 * Generates random string, with optional prefix
+	 *
+	 * @param string $prefix
+	 *
+	 * @return string
+	 */
+	protected function getRandomString($prefix = null)
+	{
+		if ($prefix !== null) {
+			$prefix = "{$prefix}_";
+		}
+
+		return $prefix . md5(uniqid(mt_rand(), true));
 	}
 
 }
