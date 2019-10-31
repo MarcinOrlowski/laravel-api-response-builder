e8b92d2c6dc6af445bc640d2b03d8183885a7aed
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2019-10-31 15:37:25 -0700

    Added Validator helper methods

--- src/Validator.php
+++ src/Validator.php
@@ -15,29 +15,45 @@ namespace MarcinOrlowski\ResponseBuilder;
  */
 class Validator
 {
+	public const TYPE_STRING  = 'string';
+	public const TYPE_INTEGER = 'integer';
+	public const TYPE_BOOL    = 'boolean';
+
 	/**
 	 * Checks if given $val is of type integer
 	 *
 	 * @param string $key Name of the key to be used if exception is thrown.
-	 * @param mixed  $val Value to validate.
+	 * @param mixed  $var Data to validated.
 	 *
 	 * @return void
 	 *
 	 * @throws \InvalidArgumentException
 	 */
-	public static function assertInt(string $key, $val): void
+	public static function assertInt(string $key, $var): void
 	{
-		if (!is_int($val)) {
-			$msg = sprintf('"%s" must be an integer (%s given)', $key, gettype($val));
-			throw new \InvalidArgumentException($msg);
-		}
+		self::assertType($key, $var, [self::TYPE_INTEGER]);
 	}
 
 	/**
-	 * @param string $key
-	 * @param mixed  $var
-	 * @param int    $min
-	 * @param int    $max
+	 * Checks if given $val is of type string
+	 *
+	 * @param string $key Name of the key to be used if exception is thrown.
+	 * @param mixed  $var Data to validated.
+	 *
+	 * @return void
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public static function assertString(string $key, $var): void
+	{
+		self::assertType($key, $var, [self::TYPE_STRING]);
+	}
+
+	/**
+	 * @param string $key Name of the key to be used if exception is thrown.
+	 * @param mixed  $var Data to validated.
+	 * @param int    $min Min allowed value (inclusive)
+	 * @param int    $max Max allowed value (inclusive)
 	 *
 	 * @return void
 	 *
@@ -56,6 +72,26 @@ class Validator
 		if (($min > $var) || ($var > $max)) {
 			throw new \InvalidArgumentException(
 				sprintf('Invalid value of "%s" (%d). Must be between %d-%d inclusive.', $key, $var, $min, $max));
+		}
+	}
+
+	/**
+	 * Checks if $item (of name $key) is of type that is include in $allowed_types.
+	 *
+	 * @param string $key
+	 * @param mixed  $var
+	 * @param array[string]  $allowed_types
+	 *
+	 * @return void
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public static function assertType(string $key, $var, array $allowed_types): void
+	{
+		$type = gettype($var);
+		if (!in_array($type, $allowed_types)) {
+			$msg = sprintf('"%s" must be one of allowed types: %s (%s given)', $key, implode(', ', $allowed_types), gettype($var));
+			throw new \InvalidArgumentException($msg);
 		}
 	}
 }
