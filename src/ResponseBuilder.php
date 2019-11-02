3095ee57673d0771e789fdf9fe8058ff17b43ab6
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2019-11-01 18:37:26 -0700

    Fixes getMessageForApiCode() arg initialized in wrong place.

--- src/ResponseBuilder.php
+++ src/ResponseBuilder.php
@@ -455,6 +455,7 @@ class ResponseBuilder
 			$key = BaseApiCodes::getCodeMessageKey($fallback_code);
 		}
 
+		$placeholders = $placeholders ?? [];
 		if (!array_key_exists('api_code', $placeholders)) {
 			$placeholders['api_code'] = $api_code;
 		}
@@ -493,14 +494,12 @@ class ResponseBuilder
 			$data = (object)$data;
 		}
 
-		$placeholders = $placeholders ?? [];
-
 		// get human readable message for API code or use message string (if given instead of API code)
-		$message = $msg_or_api_code;
-		if (is_int($message)) {
+		if (is_int($msg_or_api_code)) {
 			$message = self::getMessageForApiCode($success, $msg_or_api_code, $placeholders);
 		} else {
-			Validator::assertString('message', $message);
+			Validator::assertString('message', $msg_or_api_code);
+			$message = $msg_or_api_code;
 		}
 
 		/** @noinspection PhpUndefinedClassInspection */
