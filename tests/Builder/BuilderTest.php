041ddd1ad1c77daf8266bce07b0378d417399768
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2019-11-07 14:46:53 -0800

    Added testWithHttpHeaders() Builder test

--- tests/Builder/BuilderTest.php
+++ tests/Builder/BuilderTest.php
@@ -27,16 +27,48 @@ class BuilderTest extends TestCase
      */
     public function testSuccess(): void
     {
-        $builder = Builder::success();
+        $expected_api_code = BaseApiCodes::OK();
 
+        $builder = Builder::success($expected_api_code);
         $this->assertInstanceOf(Builder::class, $builder);
         $this->response = $builder->build();
 
-        $expected_api_code = BaseApiCodes::OK();
         $j = $this->getResponseSuccessObject();
 
         $this->assertNull($j->data);
         $this->assertEquals(\Lang::get(BaseApiCodes::getCodeMessageKey($expected_api_code)), $j->message);
     }
 
+    /**
+     * Checks if custom headers are properly used in the response.
+     */
+    public function testWithHttpHeaders(): void
+    {
+        $key1 = $this->getRandomString('key1');
+        $val1 = $this->getRandomString('val1');
+        $key2 = $this->getRandomString('key2');
+        $val2 = $this->getRandomString('val2');
+        $key3 = $this->getRandomString('key3');
+        $val3 = $this->getRandomString('val3');
+        $key4 = $this->getRandomString('key4');
+        $val4 = $this->getRandomString('val4');
+
+        $headers = [
+            $key1 => $val1,
+            $key2 => $val2,
+            $key3 => $val3,
+            $key4 => $val4,
+        ];
+
+        $builder = Builder::success();
+        $this->assertInstanceOf(Builder::class, $builder);
+        $this->response = $builder
+            ->withHttpHeaders($headers)
+            ->build();
+
+        foreach($headers as $key=>$val) {
+            $this->assertTrue($this->response->headers->has($key));
+            $this->assertEquals($val, $this->response->headers->get($key));
+        }
+    }
 }
