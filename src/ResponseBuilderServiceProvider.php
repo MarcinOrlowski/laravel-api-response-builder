e6866fd1d4fd955de4c67f38efd52045e441c68f
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2020-10-24 12:04:25 +0200

    Presence of configuration "converter/classes" array is now mandatory.

--- src/ResponseBuilderServiceProvider.php
+++ src/ResponseBuilderServiceProvider.php
@@ -22,57 +22,61 @@ namespace MarcinOrlowski\ResponseBuilder;
  */
 
 use Illuminate\Support\ServiceProvider;
+use MarcinOrlowski\ResponseBuilder\Exceptions\IncompleteConfigurationException;
 
 class ResponseBuilderServiceProvider extends ServiceProvider
 {
-    protected $config_files = [
-        'response_builder.php',
-    ];
+	protected $config_files = [
+		'response_builder.php',
+	];
 
-    /**
-     * Register bindings in the container.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        foreach ($this->config_files as $file) {
-            $this->mergeConfigFrom(__DIR__ . "/../config/{$file}", ResponseBuilder::CONF_CONFIG);
-        }
-    }
+	/**
+	 * Register bindings in the container.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		foreach ($this->config_files as $file) {
+			$this->mergeConfigFrom(__DIR__ . "/../config/{$file}", ResponseBuilder::CONF_CONFIG);
+		}
+	}
 
-    /**
-     * Sets up package resources
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->loadTranslationsFrom(__DIR__ . '/lang', 'response-builder');
+	/**
+	 * Sets up package resources
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->loadTranslationsFrom(__DIR__ . '/lang', 'response-builder');
 
-        foreach ($this->config_files as $file) {
-            $this->publishes([__DIR__ . "/../config/{$file}" => config_path($file)]);
-        }
-    }
+		foreach ($this->config_files as $file) {
+			$this->publishes([__DIR__ . "/../config/{$file}" => config_path($file)]);
+		}
+	}
 
-    /**
-     * Merge the given configuration with the existing configuration.
-     *
-     * @param string $path
-     * @param string $key
-     *
-     * @return void
-     */
-    protected function mergeConfigFrom($path, $key)
-    {
-        $defaults = require $path;
-        $config = $this->app['config']->get($key, []);
+	/**
+	 * Merge the given configuration with the existing configuration.
+	 *
+	 * @param string $path
+	 * @param string $key
+	 *
+	 * @return void
+	 */
+	protected function mergeConfigFrom($path, $key)
+	{
+		$defaults = require $path;
+		$config = $this->app['config']->get($key, []);
 
-        $merged_config = Util::mergeConfig($defaults, $config);
+		$merged_config = Util::mergeConfig($defaults, $config);
 
-        if (\array_key_exists('converter', $merged_config)) {
-            Util::sortArrayByPri($merged_config['converter']);
-        }
+		if (!isset($merged_config['converter']['classes'])) {
+			throw new IncompleteConfigurationException(
+				sprintf('Configuration lacks "%s" array.', ResponseBuilder::CONF_KEY_CONVERTER_CLASSES));
+		}
+
+		Util::sortArrayByPri($merged_config['converter']['classes']);
 
         $this->app['config']->set($key, $merged_config);
     }
