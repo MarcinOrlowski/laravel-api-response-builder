7743f8a578692bbb5745b404426be6e7023cb5bd
Raja Munisamy <mrajasays@gmail.com>
2020-10-25 22:19:14 +1100

    fix quality review issues for last commit

--- src/Converter.php
+++ src/Converter.php
@@ -227,7 +227,7 @@ class Converter
 
                     if (!\in_array(\gettype($class_config[$key_name]), $allowed_types)) {
                         throw new Ex\InvalidConfigurationElementException(
-                            sprintf("Config '%s' for '{$class_name}' must be these types: %s.", RB::KEY_KEY,  \implode(', ', $allowed_types)));
+                            sprintf("Config '%s' for '{$class_name}' must be these types: %s.", RB::KEY_KEY, \implode(', ', $allowed_types)));
                     }
 				}
 			}
