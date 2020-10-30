96c779e2b84794014209f6f99b40c8052537c5ab
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2020-10-30 09:36:39 +0100

    Config formatting

--- config/response_builder.php
+++ config/response_builder.php
@@ -12,122 +12,126 @@
  */
 
 return [
-    /*
-    |-----------------------------------------------------------------------------------------------------------
-    | Code range settings
-    |-----------------------------------------------------------------------------------------------------------
-    */
-    'min_code'          => 100,
-    'max_code'          => 1024,
+	/*
+	|-------------------------------------------------------------------------------------------------------------------
+	| Code range settings
+	|-------------------------------------------------------------------------------------------------------------------
+	*/
+	'min_code'          => 100,
+	'max_code'          => 1024,
 
-    /*
-    |-----------------------------------------------------------------------------------------------------------
-    | Error code to message mapping
-    |-----------------------------------------------------------------------------------------------------------
-    |
-    */
-    'map'               => [
-        // YOUR_API_CODE => '<MESSAGE_KEY>',
-    ],
+	/*
+	|-------------------------------------------------------------------------------------------------------------------
+	| Error code to message mapping
+	|-------------------------------------------------------------------------------------------------------------------
+	*/
+	'map'               => [
+//         YOUR_API_CODE => '<MESSAGE_LOCALISATION_KEY>',
+	],
 
-    /*
-    |-----------------------------------------------------------------------------------------------------------
-    | Response Builder data converter
-    |-----------------------------------------------------------------------------------------------------------
-    |
-    */
-    'converter'         => [
-	    'primitives' => [
-		    /* Configuration for primitives used when such data is passed directly as payload (i.e. `success(15)`;) */
-		    'array'   => [
-			    'key' => 'values',
-		    ],
-		    'boolean' => [
-			    'key' => 'value',
-		    ],
-		    'double'  => [
-			    'key' => 'value',
-		    ],
-		    'integer' => [
-			    'key' => 'value',
-		    ],
-		    'string'  => [
-			    'key' => 'value',
-		    ],
-	    ],
+	/*
+	|-------------------------------------------------------------------------------------------------------------------
+	| Response Builder data converter
+	|-------------------------------------------------------------------------------------------------------------------
+	*/
+	'converter'         => [
+		'primitives' => [
+			/*
+			|-----------------------------------------------------------------------------------------------------------
+			| Configuration for primitives used when such data is passed directly as payload (i.e. `success(15)`;)
+			|-----------------------------------------------------------------------------------------------------------
+			*/
+			'array'   => [
+				'key' => 'values',
+			],
+			'boolean' => [
+				'key' => 'value',
+			],
+			'double'  => [
+				'key' => 'value',
+			],
+			'integer' => [
+				'key' => 'value',
+			],
+			'string'  => [
+				'key' => 'value',
+			],
+		],
 
-	    /* Object converters configuration for supported classes */
-    	'classes' => [
-	        \Illuminate\Database\Eloquent\Model::class          => [
-	            'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
-	            'key'     => 'item',
-	            'pri'     => 0,
-	        ],
-	        \Illuminate\Support\Collection::class               => [
-	            'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
-	            'key'     => 'items',
-	            'pri'     => 0,
-	        ],
-	        \Illuminate\Database\Eloquent\Collection::class     => [
-	            'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
-	            'key'     => 'items',
-	            'pri'     => 0,
-	        ],
-	        \Illuminate\Http\Resources\Json\JsonResource::class => [
-	            'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
-	            'key'     => 'item',
-	            'pri'     => 0,
-	        ],
+		/*
+		|-----------------------------------------------------------------------------------------------------------
+		| Object converters configuration for supported classes
+		|-----------------------------------------------------------------------------------------------------------
+		*/
+		'classes'    => [
+			\Illuminate\Database\Eloquent\Model::class          => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
+				'key'     => 'item',
+				'pri'     => 0,
+			],
+			\Illuminate\Support\Collection::class               => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
+				'key'     => 'items',
+				'pri'     => 0,
+			],
+			\Illuminate\Database\Eloquent\Collection::class     => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
+				'key'     => 'items',
+				'pri'     => 0,
+			],
+			\Illuminate\Http\Resources\Json\JsonResource::class => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ToArrayConverter::class,
+				'key'     => 'item',
+				'pri'     => 0,
+			],
 
-	        /*
+			/*
 			|-----------------------------------------------------------------------------------------------------------
 			| Paginators converts to objects already, so we do not array wrapping here, hence setting `key` to null
 			|-----------------------------------------------------------------------------------------------------------
 			*/
+			\Illuminate\Pagination\LengthAwarePaginator::class  => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ArrayableConverter::class,
+				'key'     => null,
+				'pri'     => 0,
+			],
+			\Illuminate\Pagination\Paginator::class             => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ArrayableConverter::class,
+				'key'     => null,
+				'pri'     => 0,
+			],
 
-	        \Illuminate\Pagination\LengthAwarePaginator::class => [
-		        'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ArrayableConverter::class,
-		        'key'     => null,
-		        'pri'     => 0,
-	        ],
-	        \Illuminate\Pagination\Paginator::class => [
-		        'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ArrayableConverter::class,
-		        'key'     => null,
-		        'pri'     => 0,
-	        ],
+			/*
+			|-----------------------------------------------------------------------------------------------------------
+			| Generic converters should have lower pri to allow dedicated ones to kick in first when class matches
+			|-----------------------------------------------------------------------------------------------------------
+			*/
+			\JsonSerializable::class                            => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\JsonSerializableConverter::class,
+				'key'     => 'item',
+				'pri'     => -10,
+			],
+			\Illuminate\Contracts\Support\Arrayable::class      => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ArrayableConverter::class,
+				'key'     => 'items',
+				'pri'     => -10,
+			],
+		],
 
-	        /*
-	        |-----------------------------------------------------------------------------------------------------------
-	        | Generic converters should have lower pri to allow dedicated ones to kick in first when class matches
-	        |-----------------------------------------------------------------------------------------------------------
-	        */
-	        \JsonSerializable::class                            => [
-	            'handler' => \MarcinOrlowski\ResponseBuilder\Converters\JsonSerializableConverter::class,
-	             'key'     => 'item',
-	            'pri'     => -10,
-	        ],
-	        \Illuminate\Contracts\Support\Arrayable::class      => [
-	            'handler' => \MarcinOrlowski\ResponseBuilder\Converters\ArrayableConverter::class,
-	            'key'     => 'items',
-	            'pri'     => -10,
-	        ],
-        ],
+	],
 
-    ],
-
-    /*
-    |-----------------------------------------------------------------------------------------------------------
-    | Exception handler error codes
-    |-----------------------------------------------------------------------------------------------------------
-    |
-    */
-    'exception_handler' => [
-	    /*
-	     * The following keys are supported for each handler specified.
-	     *   `handler`
-	     *   `pri`
-	     *   `config`
-	     *
+	/*
+	|-------------------------------------------------------------------------------------------------------------------
+	| Exception handler error codes
+	|-------------------------------------------------------------------------------------------------------------------
+	*/
+	'exception_handler' => [
+		/*
+		 * The following keys are supported for each handler specified.
+		 *   `handler`
+		 *   `pri`
+		 *   `config`
+		 *
 		 * The following keys are supported in "config" entry for each handler specified:
 		 *   `api_code`   : (int) mandatory api_code to be used for given exception
 		 *   `http_code`  : (int) optional HTTP code. If not specified, exception's HTTP status code will be used.
@@ -142,19 +146,19 @@ return [
 		 *                  message ($ex->getMessage()).
 		 */
 
-    	\Illuminate\Validation\ValidationException::class => [
-		    'handler' => \MarcinOrlowski\ResponseBuilder\ExceptionHandlers\ValidationExceptionHandler::class,
-		    'pri'     => -100,
-		    'config' => [
+		\Illuminate\Validation\ValidationException::class => [
+			'handler' => \MarcinOrlowski\ResponseBuilder\ExceptionHandlers\ValidationExceptionHandler::class,
+			'pri'     => -100,
+			'config'  => [
 //		        'api_code'  => ApiCodes::YOUR_API_CODE_FOR_VALIDATION_EXCEPTION,
 //		        'http_code' => HttpResponse::HTTP_UNPROCESSABLE_ENTITY,
-		    	],
-	    ],
+			],
+		],
 
 		\Symfony\Component\HttpKernel\Exception\HttpException::class => [
-	        'handler' => \MarcinOrlowski\ResponseBuilder\ExceptionHandlers\HttpExceptionHandler::class,
-	        'pri'     => -100,
-	        'config'  => [
+			'handler' => \MarcinOrlowski\ResponseBuilder\ExceptionHandlers\HttpExceptionHandler::class,
+			'pri'     => -100,
+			'config'  => [
 //		        HttpException::class => [
 //			        // used by unauthenticated() to obtain api and http code for the exception
 //			        HttpResponse::HTTP_UNAUTHORIZED         => [
@@ -170,47 +174,49 @@ return [
 //				        'http_code' => HttpResponse::HTTP_BAD_REQUEST,
 //			        ],
 //		        ],
-	        ],
-//	        // This is final exception handler. If ex is not dealt with yet this is its last stop.
-	        // default handler is mandatory and MUST have both `api_code` and `http_code` set.
+			],
 
-	        'default' => [
-		        'handler' => \MarcinOrlowski\ResponseBuilder\ExceptionHandlers\HttpExceptionHandler::class,
-		        'pri'     => -127,
-		        'config'  => [
+			/*
+			|-----------------------------------------------------------------------------------------------------------
+			| This is final exception handler. If ex is not dealt with yet this is its last stop.
+			| Default handler is mandatory and MUST have both `api_code` and `http_code` set.
+			|-----------------------------------------------------------------------------------------------------------
+			*/
+			'default' => [
+				'handler' => \MarcinOrlowski\ResponseBuilder\ExceptionHandlers\HttpExceptionHandler::class,
+				'pri'     => -127,
+				'config'  => [
 //			        'api_code'  => ApiCodes::YOUR_API_CODE_FOR_UNHANDLED_EXCEPTION,
 //			        'http_code' => HttpResponse::HTTP_INTERNAL_SERVER_ERROR,
-		        ],
-	        ],
-	    ],
-    ],
+				],
+			],
+		],
+	],
 
-    /*
-    |-----------------------------------------------------------------------------------------------------------
-    | data-to-json encoding options
-    |-----------------------------------------------------------------------------------------------------------
-    |
-    */
-    'encoding_options'  => JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_UNICODE,
+	/*
+	|-------------------------------------------------------------------------------------------------------------------
+	| data-to-json encoding options
+	|-------------------------------------------------------------------------------------------------------------------
+	*/
+	'encoding_options'  => JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_UNICODE,
 
-    /*
-    |-----------------------------------------------------------------------------------------------------------
-    | Debug config
-    |-----------------------------------------------------------------------------------------------------------
-    |
-    */
-    'debug'             => [
-        'debug_key'         => 'debug',
-        'exception_handler' => [
-            'trace_key'     => 'trace',
-            'trace_enabled' => env('APP_DEBUG', false),
-        ],
+	/*
+	|-------------------------------------------------------------------------------------------------------------------
+	| Debug config
+	|-------------------------------------------------------------------------------------------------------------------
+	*/
+	'debug'             => [
+		'debug_key'         => 'debug',
+		'exception_handler' => [
+			'trace_key'     => 'trace',
+			'trace_enabled' => env('APP_DEBUG', false),
+		],
 
-        // Controls debugging features of payload converter class.
-        'converter' => [
-        	// Set to true to figure out what converter is used for given data payload and why.
-        	'debug_enabled' => env('RB_CONVERTER_DEBUG', false),
-        ],
-    ],
+		// Controls debugging features of payload converter class.
+		'converter'         => [
+			// Set to true to figure out what converter is used for given data payload and why.
+			'debug_enabled' => env('RB_CONVERTER_DEBUG', false),
+		],
+	],
 
 ];
