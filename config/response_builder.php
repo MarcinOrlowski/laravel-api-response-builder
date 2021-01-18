8e8773d6a2317f2694ab3b6ca4a08df724e41825
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-01-18 01:17:34 +0100

    Bumped copyright year to 2021

--- config/response_builder.php
+++ config/response_builder.php
@@ -6,7 +6,7 @@
  * See docs/config.md for detailed documentation
  *
  * @author    Marcin Orlowski <mail (#) marcinOrlowski (.) com>
- * @copyright 2016-2020 Marcin Orlowski
+ * @copyright 2016-2021 Marcin Orlowski
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      https://github.com/MarcinOrlowski/laravel-api-response-builder
  */
