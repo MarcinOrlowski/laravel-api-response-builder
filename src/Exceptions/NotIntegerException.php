be6f7b7da96e321369a970474ee38bd331085076
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-06-20 20:22:21 +0200

    `Validator::assertIsInt()` throws now `NotIntegerException` as expected.

---
+++ src/Exceptions/NotIntegerException.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace MarcinOrlowski\ResponseBuilder\Exceptions;
+
+/**
+ * Laravel API Response Builder
+ *
+ * @package   MarcinOrlowski\ResponseBuilder
+ *
+ * @author    Marcin Orlowski <mail (#) marcinOrlowski (.) com>
+ * @copyright 2016-2021 Marcin Orlowski
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/MarcinOrlowski/laravel-api-response-builder
+ */
+final class NotIntegerException extends InvalidTypeException
+{
+	// empty
+}
